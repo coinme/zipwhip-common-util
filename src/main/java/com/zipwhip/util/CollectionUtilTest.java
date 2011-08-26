2572ab3c1a00bc87530930127facfc2ddc6661cc
Michael Smyers <msmyers@zipwhip.com>
2011-08-25 17:27:35 -0700

    added tests and fixed bug

---
+++ src/main/java/com/zipwhip/util/CollectionUtilTest.java
@@ -0,0 +1,15 @@
+package com.zipwhip.util;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Michael
+ * Date: 8/25/11
+ * Time: 5:08 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class CollectionUtilTest {
+    @org.junit.Test
+    public void testGetList() throws Exception {
+
+    }
+}
