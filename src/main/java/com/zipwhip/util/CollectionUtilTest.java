1a5767c85956eec5bb854850ceecadea39ac8568
jedhoffmann <jedhoffmann@gmail.com>
2011-08-26 16:34:43 -0700

    Removing from param Serializer.java

--- src/main/java/com/zipwhip/util/CollectionUtilTest.java
+++ src/main/java/com/zipwhip/util/CollectionUtilTest.java
@@ -5,10 +5,11 @@ package com.zipwhip.util;
  * User: Michael
  * Date: 8/25/11
  * Time: 5:08 PM
- * To change this template use File | Settings | File Templates.
+ *
  */
 public class CollectionUtilTest {
-    @org.junit.Test
+
+    //@org.junit.Test
     public void testGetList() throws Exception {
 
     }
--- src/main/java/com/zipwhip/util/Serializer.java
+++ src/main/java/com/zipwhip/util/Serializer.java
@@ -3,17 +3,15 @@ package com.zipwhip.util;
 /**
  * Created by IntelliJ IDEA. User: Michael Date: 8/19/11 Time: 4:22 PM
  * 
- * Serialize something to a string
+ * Serialize the implementing object to a string
  */
-public interface Serializer<T> {
+public interface Serializer {
 
     /**
-     * Serialize your object to this output
+     * Serialize this object to this output
      * 
-     * @param item
-     *        the thing you want to serialize
-     * @return the serialized string representation of the item
+     * @return the serialized string representation of this
      */
-    String serialize(T item);
+    String serialize();
 
 }
