d428dcdb7564d22a1acf17a6c199b85ccc3bd953
Jeremy Dinsel <jdinsel@zipwhip.com>
2011-11-02 16:25:27 -0700

    Return an empty set, not a null. This is for safety in a for( x : X) loop

--- src/main/java/com/zipwhip/util/GenericLocalDirectory.java
+++ src/main/java/com/zipwhip/util/GenericLocalDirectory.java
@@ -1,6 +1,11 @@
 package com.zipwhip.util;
 
-import java.util.*;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
 
 /**
  * Created by IntelliJ IDEA.
@@ -12,98 +17,98 @@ import java.util.*;
  */
 public class GenericLocalDirectory<TKey, TValue> extends GenericDirectory<TKey, TValue> implements LocalDirectory<TKey, TValue> {
 
-    protected static final Comparator COMPARATOR = new HashCodeComparator();
+	protected static final Comparator COMPARATOR = new HashCodeComparator();
 
-    protected Factory<Collection<TValue>> factory = null;
-    protected Map<TKey, Collection<TValue>> data = null;
+	protected Factory<Collection<TValue>> factory = null;
+	protected Map<TKey, Collection<TValue>> data = null;
 
-    public GenericLocalDirectory(Factory<Collection<TValue>> factory) {
-        this.factory = factory;
-    }
+	public GenericLocalDirectory(Factory<Collection<TValue>> factory) {
+		this.factory = factory;
+	}
 
-    @Override
-    public Set<TKey> keySet() {
-        if (data == null) {
-            return null;
-        }
+	@Override
+	public Set<TKey> keySet() {
+		if (data == null) {
+			return Collections.emptySet();
+		}
 
-        return data.keySet();
-    }
+		return data.keySet();
+	}
 
-    @Override
-    public boolean isEmpty() {
-        if (data == null) {
-            return true;
-        }
+	@Override
+	public boolean isEmpty() {
+		if (data == null) {
+			return true;
+		}
 
-        return data.isEmpty();
-    }
+		return data.isEmpty();
+	}
 
-    @Override
-    public void clear() {
-        if (data == null) {
-            return;
-        }
+	@Override
+	public void clear() {
+		if (data == null) {
+			return;
+		}
 
-        data.clear();
-    }
+		data.clear();
+	}
 
-    @Override
-    public Collection<TValue> get(TKey key) {
-        if (data == null){
-            return null;
-        }
+	@Override
+	public Collection<TValue> get(TKey key) {
+		if (data == null){
+			return null;
+		}
 
-        return data.get(key);
-    }
+		return data.get(key);
+	}
 
-    @Override
-    protected void removeFromStore(TKey key) {
-        if (data == null){
-            return;
-        }
+	@Override
+	protected void removeFromStore(TKey key) {
+		if (data == null){
+			return;
+		}
 
-        data.remove(key);
-    }
+		data.remove(key);
+	}
 
-    @Override
-    protected Collection<TValue> getOrCreateCollection(TKey key) {
-        if (data == null) {
-            synchronized (this) {
-                if (data == null) {
-                    data = createStore();
-                }
-            }
-        }
+	@Override
+	protected Collection<TValue> getOrCreateCollection(TKey key) {
+		if (data == null) {
+			synchronized (this) {
+				if (data == null) {
+					data = createStore();
+				}
+			}
+		}
 
-        Collection<TValue> collection = data.get(key);
+		Collection<TValue> collection = data.get(key);
 
-        if (collection == null) {
-            synchronized (data) {
-                collection = data.get(key);
-                if (collection == null) {
-                    try {
-                        collection = factory.create();
-                    } catch (Exception e) {
-                        // TODO: figure out if we should add an exception to the .add() method
-                        throw new RuntimeException("Cannot create collection via factory", e);
-                    }
+		if (collection == null) {
+			synchronized (data) {
+				collection = data.get(key);
+				if (collection == null) {
+					try {
+						collection = factory.create();
+					} catch (Exception e) {
+						// TODO: figure out if we should add an exception to the .add() method
+						throw new RuntimeException("Cannot create collection via factory", e);
+					}
 
-                    data.put(key, collection);
-                }
-            }
-        }
+					data.put(key, collection);
+				}
+			}
+		}
 
-        return collection;
-    }
+		return collection;
+	}
 
-    protected Map<TKey, Collection<TValue>> createStore() {
-        return Collections.synchronizedMap(new TreeMap<TKey, Collection<TValue>>(COMPARATOR));
-    }
+	protected Map<TKey, Collection<TValue>> createStore() {
+		return Collections.synchronizedMap(new TreeMap<TKey, Collection<TValue>>(COMPARATOR));
+	}
 
-    @Override
-    protected void onDestroy() {
-        data.clear();
-    }
+	@Override
+	protected void onDestroy() {
+		data.clear();
+	}
 
 }
