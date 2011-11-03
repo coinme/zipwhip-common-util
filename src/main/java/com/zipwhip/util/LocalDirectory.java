37dfe8ed7653ddd048087d64a95e40be5d64f346
Jeremy Dinsel <jdinsel@zipwhip.com>
2011-11-03 16:09:00 -0700

    Implement serializable for insertion into cache(s)

--- src/main/java/com/zipwhip/util/LocalDirectory.java
+++ src/main/java/com/zipwhip/util/LocalDirectory.java
@@ -1,5 +1,6 @@
 package com.zipwhip.util;
 
+import java.io.Serializable;
 import java.util.Set;
 
 /**
@@ -10,25 +11,25 @@ import java.util.Set;
  * <p/>
  * For local in JVM directories
  */
-public interface LocalDirectory<TKey, TValue> extends Directory<TKey, TValue> {
+public interface LocalDirectory<TKey, TValue> extends Directory<TKey, TValue>, Serializable {
 
-    /**
-     * Returns a set of the keys that have been used in the past. There is no guarantee that each key has values.
-     *
-     * For example, clearing a collection does not remove the key from the set. If it does, that's an implementation
-     * detail and not a guarantee across implementations.
-     *
-     * @return
-     */
-    Set<TKey> keySet();
+	/**
+	 * Returns a set of the keys that have been used in the past. There is no guarantee that each key has values.
+	 *
+	 * For example, clearing a collection does not remove the key from the set. If it does, that's an implementation
+	 * detail and not a guarantee across implementations.
+	 *
+	 * @return
+	 */
+	Set<TKey> keySet();
 
-    /**
-     * Returns if the directory has no data.
-     *
-     * @return
-     */
-    boolean isEmpty();
+	/**
+	 * Returns if the directory has no data.
+	 *
+	 * @return
+	 */
+	boolean isEmpty();
 
-    void clear();
+	void clear();
 
 }
