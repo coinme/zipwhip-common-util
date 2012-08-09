7683a48840f2bb7bff42dd30c7a0061d7b188be5
Rhomboidal <cerickson@zipwhip.com>
2012-08-08 17:06:11 -0700

    * Added additional documentation to ReliableDeliveryWorker.

--- src/main/java/com/zipwhip/reliable/ReliableDeliveryWorker.java
+++ src/main/java/com/zipwhip/reliable/ReliableDeliveryWorker.java
@@ -17,8 +17,16 @@ public interface ReliableDeliveryWorker {
 
     /**
      * Handles the processing of the work unit.
-     * @param parameters
-     * @return
+     * @param parameters The parameters necessary to process the supplied work.  In situations where said parameters
+     *                   are sent into by way of the ReliableDeliveryService, all parameters have been pre-validated
+     *                   before being submitted to this method.
+     * @return A ReliableDeliveryResult object, which designates how the supplied work unit should be re-attempted.
+     *         Allowed values are OPERATION_SUCCESSFUL, OPERATION_FAILED_DO_NOT_REATTEMPT, OPERATION_FAILED_REATTEMPT_BLOCKING,
+     *         and OPERATION_FAILED_REATTEMPT_NOT_BLOCKING.  Returning a null, or any other value will cause the supplied
+     *         work unit to be classified as having broken a failsafe, and will not be re-attempted.
+     *         In addition, the ReliableDeliveryWorker should make all attempts to catch exceptions internally.  In situations
+     *         where an unchecked exception is thrown, the supplied work unit is classified as having broken a failsafe, and
+     *         will not be reattempted.
      */
     public ReliableDeliveryResult executeWork(ReliableDeliveryWorkParameters parameters);
 
