abc219cd678d02dd946a7d1e704f1f7680246695
Russ Wirtz <rwirtz@zipwhip.com>
2012-09-11 16:38:26 -0700

    Allow the ExponentialBackoffRetryStrategy to be created with defaults

--- src/main/java/com/zipwhip/reliable/retry/ExponentialBackoffRetryStrategy.java
+++ src/main/java/com/zipwhip/reliable/retry/ExponentialBackoffRetryStrategy.java
@@ -13,10 +13,18 @@ public class ExponentialBackoffRetryStrategy implements RetryStrategy {
     private int startingInterval;
     private double retryMultiplier;
 
+    private static final int DEFAULT_MAX_ATTEMPT_COUNT = 10;
+    private static final int DEFAULT_STARTING_INTERVAL = 1000;
+    private static final double DEFAULT_RETRY_MULTIPLIER = 10;
+
+    public ExponentialBackoffRetryStrategy() {
+        this(DEFAULT_MAX_ATTEMPT_COUNT, DEFAULT_STARTING_INTERVAL, DEFAULT_RETRY_MULTIPLIER);
+    }
+
     /**
      * @param maxAttemptCount The maximum number of times that this operation should be re-attempted.  A non-positive value will be interpreted to be 1.
      * @param startingInterval The interval returned for getNextRetryInterval where failedAttemptCount == 1.
-     * @param retryMultiplier The amount by which we should multiply each subsequent interval after the first.  
+     * @param retryMultiplier The amount by which we should multiply each subsequent interval after the first.
      *                        After the first attempt, interval is (startingInterval).  After the second attempt, interval is (startingInterval * retryMultiplier).  Third attempt is (startingInterval * retryMultiplier^2)
      *                        NOTE: In situations where the retry multiplier is less than one, we change the value to one (To prevent situations where the interval gets shorter and shorter after each call).
      */
