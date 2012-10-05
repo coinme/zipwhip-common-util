f53ac3ab101a5556046e97f369fba9b3d389bfb1
Michael Smyers <msmyers@zipwhip.com>
2012-10-05 14:41:31 -0700

    Repackaging classes to appropriate project

---
+++ src/main/java/com/zipwhip/util/Converter.java
@@ -0,0 +1,18 @@
+package com.zipwhip.util;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Michael
+ * Date: 1/28/11
+ * Time: 11:40 PM
+ *
+ * Converts out of and into bytes.
+ *
+ */
+public interface Converter<TSource, TUnderlying> {
+
+    TUnderlying convert(TSource source) throws Exception;
+
+    TSource restore(TUnderlying underlying) throws Exception;
+
+}
---
+++ src/main/java/com/zipwhip/util/Formatter.java
@@ -0,0 +1,15 @@
+package com.zipwhip.util;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Michael
+ * Date: 10/4/12
+ * Time: 3:29 PM
+ *
+ * Takes your input string and creates a format
+ */
+public interface Formatter<T> {
+
+    T format(T input);
+
+}
